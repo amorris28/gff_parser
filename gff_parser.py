4131b579e178b2d68341a2e8cafdc5645baa1fc4
Andrew Morris <amorris3@uoregon.edu>
2022-03-24 13:34:13 -0700

    Make gff_parser work on prodigal files

--- gff_parser.py
+++ gff_parser.py
@@ -12,29 +12,21 @@ import sys
 from collections import Counter
 
 #parse the arguments
-parser = argparse.ArgumentParser(description="""Parse Prokka annotated genome/metagenome to add external gene calls and functions to anvi'o.
-                    Input annotation in GFF3 format, outputs are tab-delimited text files, one for gene calls and one for annotations""")
-parser.add_argument('gff_file', metavar='GFF3', help='Annotation file from Prokka in GFF3 format')
+parser = argparse.ArgumentParser(description="""Parse Prodigal to add external gene calls to anvi'o.
+                    Input gene calls in GFF3 format, output in tab-delimited text file""")
+parser.add_argument('gff_file', metavar='GFF3', help='Gene call file from Prodigal in GFF3 format')
 parser.add_argument('--gene-calls', default='gene_calls.txt', help='Output: External gene calls (Default: gene_calls.txt)')
-parser.add_argument('--annotation', default='gene_annot.txt', help='Output: Functional annotation for external gene calls (Default: gene_annot.txt)')
-parser.add_argument('--process-all', default=False, action="store_true", help="Prodigal returns anything it finds, including tRNAs and\
-                    other genetic structures that are not nessecarily translatable, and can cause downstream issues especially if you would like to\
-                    use your annotations in contigs databases for pangenomic analyses. As a precation this script only recovers open reading frames\
-                    identifie dby Prodigal. Using this flag, you can import everything.")
-parser.add_argument('--source', default='Prokka', help='Source of gene calls (Prokka or IMG) (Default: Prokka)')
+parser.add_argument('--source', default='Prodigal', help='Source of gene calls (Prodigal) (Default: Prodigal)')
 
 args = parser.parse_args()
 
 #Input file and output files
 GFF = args.gff_file
 OUT_CDS = open(args.gene_calls, 'w')
-OUT_ANNO = open(args.annotation, 'w')
 
 #Check gene caller
 SOURCE = args.source
-if SOURCE == 'Prokka':
-    SEP = ':'
-elif SOURCE == 'IMG':
+if SOURCE == 'Prodigal':
     SEP = ' '
 else:
     print(SOURCE, "is not an available gene caller.")
@@ -45,11 +37,9 @@ db = gffutils.create_db(GFF, ':memory:')
 
 #Print headers for anvi'o
 OUT_CDS.write("gene_callers_id\tcontig\tstart\tstop\tdirection\tpartial\tcall_type\tsource\tversion\n")
-OUT_ANNO.write("gene_callers_id\tsource\taccession\tfunction\te_value\n")
 
 #running gene ID and a trumped-up e-value for the gene calls.
 gene_id = 1
-e_value = "0"
 
 # keping track of things we haven't processed
 feature_types = Counter()
@@ -62,11 +52,6 @@ for feature in db.all_features():
     total_num_features += 1
     # determine source
     source, version = feature.source.split(SEP, 1)
-
-    # move on if not Prodigal, unless the user wants it badly
-    if not args.process_all:
-        if source != 'Prodigal':
-            continue
 
     start = feature.start - 1
     stop = feature.stop
@@ -92,21 +77,6 @@ for feature in db.all_features():
     except KeyError:
         gene_acc = ""
 
-    # if a feature is missing both, move on.
-    if 'product' not in feature.attributes.keys() and 'note' not in feature.attributes.keys():
-        features_missing_product_or_note += 1
-        continue
-
-    try:
-        product = feature.attributes['product'][0]
-    except KeyError:
-        product = feature.attributes['note'][0]
-
-    # skip if hypotethical proiten:
-    if product == 'hypothetical protein':
-        product = ""
-        gene_acc = ""
-
     # determine direction
     if feature.featuretype=='repeat_region':
         direction='f'
@@ -117,8 +87,6 @@ for feature in db.all_features():
             direction='r'
 
     OUT_CDS.write('%d\t%s\t%d\t%d\t%s\t%s\t%d\t%s\t%s\n' % (gene_id, feature.seqid, start, stop, direction, partial, call_type, source, version))
-    OUT_ANNO.write('%d\t%s:%s\t%s\t%s\t%s\n' % (gene_id, SOURCE, source, gene_acc, product, e_value))
-
     gene_id = gene_id + 1
 
 print(f"Done. All {total_num_features} have been processed succesfully. There were {call_types['CDS']} coding "
